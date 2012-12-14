f43626f3b8e90402e2e17cf4a68acc8a2d1b9e3d
Andrew Dudley <adudley@gmail.com>
2012-12-13 16:08:59 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Addapp2::Application.config.secret_token = 'dd29f72d9921118d70fc2c48b7b968a4952a84d896bdba734e775b0f018cbf4c85acf7e2ec0a1f16372dc5a7d249dfeeae4645911cfb77e59e7267021f596171'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Addapp2::Application.config.session_store :cookie_store, key: '_addapp2_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Addapp2::Application.config.session_store :active_record_store
